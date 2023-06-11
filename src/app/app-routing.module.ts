b296a2c73f09f6b401b1e1fbb1c2bf607acd8260
Vimal Bhalani <vimalbhalani77@gmail.com>
2023-06-11 13:52:16 +0530

    add routing fir svg chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SvgchartComponent } from './svg-chart/svg-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chart', component: SvgchartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
